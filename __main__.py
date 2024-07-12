418063ef665225792a00fb344a6fe9d5c5614d43
iris <snazzsinclair@gmail.com>
2024-07-12 15:45:23 -0500

    commit before i rework all the scripts

--- __main__.py
+++ __main__.py
@@ -3,8 +3,28 @@ import scripts
 import os
 
 args = sys.argv
+sdkdir = ''
+if sys.platform == 'Windows':
+    prefix = '%AppData%'
+else:
+    prefix = '~'
+datapath = prefix + '/.ftcandroid/'
+confpath = datapath + 'sdkpath.conf'
+if os.path.exists(confpath):
+    with open(confpath) as f:
+        sdkdir = str(f.read().splitlines()[0]).removeprefix('sdkdir=')
+else:
+    os.mkdir(datapath)
+
+os.system('export ANDROID_HOME=' + sdkdir)
 
 if args[1] == 'setup':
-    scripts.gradle.setup()
-elif args[2] == 'sync':
+    scripts.android.setup()
+elif args[1] == 'sync':
     scripts.gradle.sync()
+elif args[1] == 'set':
+    if args[2] == 'sdkdir':
+        with open(confpath, 'w+') as f:
+            content = f.readlines()
+            content[0] = 'sdkdir=' + args[3]
+            f.writelines(content)
--- scripts.py
+++ scripts.py
@@ -4,38 +4,10 @@ import sys
 
 class gradle:
     @staticmethod
-    def sdksetup():
-        if sys.platform == 'Windows':
-            platform = 'win'
-            sdkdir = 'C:\\....'
-        else:
-            platform = '*nix'
-            sdkdir = '/opt/android-sdk'
+    def sync():
+        os.system('./gradlew sync')
 
-        try:
-            subprocess.check_output('sdkmanager --version', shell=True, text=True)
-        except:
-            print('Android sdkmanager not found. Please install it and try again')
-            exit()
-
-        if platform == '*nix'
-            print('Superuser is required for \'mkdir /opt/android-sdk\' and \'chmod 777 /opt/android-sdk\'')
-            os.system('sudo mkdir /opt/android-sdk')
-            os.system('sudo chmod 777 /opt/android-sdk')
-            print('Made /opt/android-sdk readable and writiable for all')
-
-            print('Attempting to install the android sdk')
-            os.system('sudo sdkmanager "platforms;android-29"')
-
-        print('Please accept the licence agreements for the Android platform')
-        os.system('sdkmanager --licenses')
-
-        print('Creating \'local.properties\'')
-        with open('local.properties', 'w') as f:
-            f.write('sdk.dir=/opt/android-sdk/platforms/android-29')
-
-        print('Android SDK is set up!')
-
+class android():
     @staticmethod
     def adbsetup():
         if sys.platform == 'Windows':
@@ -55,9 +27,41 @@ class gradle:
         os.system('sdkmanager --licenses')
 
     @staticmethod
+    def sdksetup():
+        if sys.platform == 'Windows':
+            platform = 'win'
+            sdkdir = 'C:\\....'
+        else:
+            platform = '*nix'
+            sdkdir = '/opt/android-sdk'
+
+        try:
+            subprocess.check_output('sdkmanager --version', shell=True, text=True)
+        except:
+            print('Android sdkmanager not found. Please install it and try again')
+            exit()
+
+        if platform == '*nix'
+            if not os.path.exists('~/.ftcandroid'):
+                os.mkdir('~/.ftcandroid')
+                os.mkdir('~/.ftcandroid/sdkmanager')
+            os.system('git clone https://gitlab.com/fdroid/sdkmanager.git ~/.ftcandroid/sdkmanager')
+            print('Attempting to install the android sdk')
+            os.system('sudo sdkmanager "platforms;android-29"')
+
+        print('Please accept the licence agreements for the Android platform')
+        os.system('sdkmanager --licenses')
+
+        print('Creating \'local.properties\'')
+        with open('local.properties', 'w') as f:
+            f.write('sdk.dir=/opt/android-sdk/platforms/android-29')
+
+        print('Android SDK is set up!')
+
+    @staticmethod
     def setup():
-        gradle.sdksetup()
-        gradle.adbsetup()
+        android.sdksetup()
+        android.adbsetup()
         platform = sys.platform
         if platform == 'Linux' or 'Darwin':
             with open('~/.android-sdk-locaiton.info', 'w') as f:
@@ -66,8 +70,3 @@ class gradle:
             os.mkdir('%AppData%/local/ftc-android')
             with open('%AppData%/local/ftc-android/android-sdk-location.info', 'w') as f:
                 f.write('C://...')
-
-
-    @staticmethod
-    def sync():
-        os.system('./gradlew sync')
